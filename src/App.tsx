d8856fd9666a67fc45135293932aa280c508cf61
Greta <asth0010@elev.techcollege.dk>
2026-01-28 10:31:23 +0100

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "./pages/home/home";
+import { MainLayout } from "./layouts/mainlayout";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<div>About</div>} />
+            <Route path="/posters" element={<div>Posters</div>} />
+            <Route path="/contact" element={<div>Contact</div>} />
+            <Route path="/login" element={<div>Login</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
